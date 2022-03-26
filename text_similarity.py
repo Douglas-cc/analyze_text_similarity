1b8c782321cc3a76fb669ab15ab0bdd5b01ff616
Douglas-cc <douglasdasilvasousa8@gmail.com>
2022-03-26 13:56:34 -0300

    Ajustes nas função de matrix de similaridade

--- text_similarity.py
+++ text_similarity.py
@@ -7,48 +7,39 @@ from jellyfish import(
     jaro_similarity
 )
 
-
 def creat_class(value_similar, bins, labels):
    return pd.cut(value_similar, bins = bins, labels = labels).astype('string')
 
-def remove_stowords(list_strings, stopwords):
+def remove_stopwords(list_strings, stopwords):
     output = [t for t in list_strings.split(' ') if t not in stopwords]
     return " ".join(output)
 
 def jaccard_similarity(f1, f2):
     return len(set(f1).intersection(set(f2)))/len(set(f1).union(set(f2)))
 
-def match_rating_matrix(list_strings):
-    size = list_strings.shape[0]
-    
-    matrix = np.empty((list_strings.shape[0], list_strings.shape[0]))
-    matrix[:] = np.NaN
-    
-    for x in range(size):
-        for y in range(x, size):
-            matrix[x][y] = match_rating_comparison(list_strings[x], list_strings[y])
-            
-    return matrix
-
 def similarity_matrix(list_strings, metric):  
     size = list_strings.shape[0]
-        
+    
     matrix = np.empty((list_strings.shape[0], list_strings.shape[0]))
     matrix[:] = np.NaN
     
     if metric == 'jaro_winkler':
         for x in range(size):
             for y in range(x, size):
-                matrix[x][y] = jaro_winkler_similarity(list_strings[x], list_strings[y])
-
-    if metric == 'jaro':
+                matrix[x][y] = jaro_winkler_similarity(list_strings[x], list_strings[y])        
+    elif metric == 'jaro':
         for x in range(size):
             for y in range(x, size):
-                matrix[x][y] = jaro_similarity(list_strings[x], list_strings[y])
-                
-    if metric == 'jaccard':
+                matrix[x][y] = jaro_similarity(list_strings[x], list_strings[y])   
+    elif metric == 'jaccard':
         for x in range(size):
             for y in range(x, size):
                 matrix[x][y] = jaccard_similarity(list_strings[x], list_strings[y])
+    elif metric == 'match_ration':    
+        for x in range(size):
+            for y in range(x, size):
+                matrix[x][y] = match_rating_comparison(list_strings[x], list_strings[y])
+    else:
+        print('Erro no parametro de metrica')
                 
     return matrix
